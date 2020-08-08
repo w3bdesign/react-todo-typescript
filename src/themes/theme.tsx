d6025219b364c6b1284908caf67f79a8f749605a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-08-08 15:28:40 +0200

    Fix Jest errors by including Easy Peasy inside the test file

--- src/themes/theme.tsx
+++ src/themes/theme.tsx
@@ -1,8 +1,8 @@
 import red from '@material-ui/core/colors/red';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, Theme } from '@material-ui/core/styles';
 
 // A custom theme for this app
-const theme = createMuiTheme({
+const theme:Theme = createMuiTheme({
   palette: {
     primary: {
       main: '#556cd6',
