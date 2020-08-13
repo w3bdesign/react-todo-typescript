9cee859aaefeb32502992e41e452d4ff6d1155cb
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-08-13 03:25:33 +0200

    Add more types

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,30 +1,35 @@
-import { createStore, action } from 'easy-peasy';
+import { createStore, action, Action } from 'easy-peasy';
 
 import INITIAL_STATE from '../const/INITIAL_STATE';
+import { BodyRender } from './types';
 
 // https://easy-peasy.now.sh/docs/typescript-tutorial/create-your-store.html
 
 // TODO Add better typing here
 
-const store = createStore({
-  todos: {
-    items: INITIAL_STATE,
-    add: action((state, payload) => {
-      // state.todos.items.push(payload);
-      console.log('Add State from Easy Peasy: ');
-      console.log(state);
-    }),
-    complete: action((state, payload) => {
-      // state.todos.items.splice(payload);
-      console.log('Complete State from Easy Peasy: ');
-      console.log(state);
-    }),
-    delete: action((state, payload) => {
-      // state.todos.items.splice(payload);
-      console.log('Delete State from Easy Peasy: ');
-      console.log(state);
-    }),
-  },
+
+
+const store:any = createStore({
+  items: INITIAL_STATE,
+  addTodo: action((state, payload) => {
+    // state.todos.items.push(payload);
+    // state.push(payload);
+    console.log('Add State from Easy Peasy: ');
+    console.log(state);
+    console.log(payload);
+  }),
+  completeTodo: action((state, payload) => {
+    // state.todos.items.splice(payload);
+    console.log('Complete State from Easy Peasy: ');
+    console.log(state);
+    console.log(payload);
+  }),
+  deleteTodo: action((state, payload) => {
+    // state.todos.items.splice(payload);
+    console.log('Delete State from Easy Peasy: ');
+    console.log(state);
+    console.log(payload);
+  }),
 });
 
 export default store;
